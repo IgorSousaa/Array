f0b7de813110c03add06f9c0a88294c959809ebb
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-26 16:04:34 -0300

    Atividade de Array

---
+++ script.js
@@ -0,0 +1,29 @@
+// Atividade Array 
+
+let familia = ["Igor","Marlene","Iury","Camila","Italo"];
+console.log(familia.sort());
+
+// ===================================================================
+
+const adicionar = familia.unshift("Alfredo");
+console.log(familia);
+
+// ===================================================================
+
+const remover = familia.pop();
+console.log(familia)
+
+// ===================================================================
+
+const adicionarNomes = familia.push ("Isabella", "Eduarda");
+console.log(familia);
+
+// ===================================================================
+
+const removerNome = familia.shift();
+console.log(familia)
+
+// ===================================================================
+
+let numbers = [7,5,6,3,8,9,2,1,4];
+console.log(numbers.sort());
